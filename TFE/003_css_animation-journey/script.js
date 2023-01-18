d38e5e35e68356137d652c71a56ed7808dd7d8e1
efp2007 <efp2007@form.efp.local>
2023-01-18 17:00:52 +0100

    class end

--- TFE/003_css_animation-journey/script.js
+++ TFE/003_css_animation-journey/script.js
@@ -1,6 +1,28 @@
 const sidebarBtnToggle = document.getElementById("sidebarBtnToggle");
-// ajouter le script pour ajouter une class "open" pour la sidebar
+const sidebar = document.getElementById('sidebar');
+let isShown = false;
 
+sidebarBtnToggle.addEventListener('click', () => {
+    isShown = !isShown;
+    sidebar.classList.add('hide-sidebar');
+
+    if (isShown) {
+        sidebarBtnToggle.classList.add('active');
+        sidebar.classList.replace('hide-sidebar', 'display-sidebar');
+    } else {
+        sidebarBtnToggle.classList.remove('active');
+        sidebar.classList.replace('display-sidebar', 'hide-sidebar');
+    }
+});
 
 const modalBtn = document.getElementById("modalBtn");
-// ajouter le script pour ajouter une class "open" pour la modal
+const closeModalBtn = document.getElementById('clode-modal-btn');
+const modal = document.getElementById('modal-element');
+
+modalBtn.addEventListener('click', () => {
+    modal.classList.add('display');
+});
+
+closeModalBtn.addEventListener('click', () => { 
+    modal.classList.remove('display');
+});
